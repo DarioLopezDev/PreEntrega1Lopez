bbf4070d53abfbc34783e107bc3820aa1fe19922
Dario Lopez <dariolopezdeveloper@gmail.com>
2024-05-06 05:31:57 -0300

    arreglado pero no funciona la pagina

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+    const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
+    return (
+      <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
+        <Nav.Item>
+          <Nav.Link eventKey="1" href="#/home">
+            Home
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="2" title="Item">
+            Ofertas
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="3" disabled>
+            Contacto
+          </Nav.Link>
+        </Nav.Item>
+        <NavDropdown title="Categorias" id="nav-dropdown">
+          <NavDropdown.Item eventKey="4.1">Gaseosas</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.2">Cervezas</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.3">Aperitivos</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item eventKey="4.4">Vinos</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    );
+  }
 
 export default NavBar
