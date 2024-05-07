5fe96e673fc76d9809af288ca8215229eccb7f75
Dario Lopez <dariolopezdeveloper@gmail.com>
2024-05-07 18:18:52 -0300

    PreEntrega1ListaLopezD

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from "./assets/img/logo.png"
 import cartWidget from "./assets/img/cart3.svg"
+import circleNumber from "./assets/img/circle-fill.svg"
 import './App.css'
 import NavBar from "./components/NavBar.jsx";
+import ItemListContainer from './components/ItemListContainer.jsx';
 
 
 function App() {
   return (
+    <>
     <nav className='navbartop'>
       <div className='logoPrincipal'>
         <img src={logo} alt="logoPrincipal" />
@@ -17,9 +20,16 @@ function App() {
         </Router>
       </div>
       <div className='cartWidget'>
-        <img src={cartWidget} alt="cartwidget" />
+        <img src={cartWidget} alt="cartwidget"/>
+        <img src={circleNumber} alt="circleNumber"/>
       </div>
     </nav>
+    <div className='inventario'>
+      <Router>
+        <ItemListContainer/>
+      </Router>
+    </div>
+    </>
   )
 }
 
